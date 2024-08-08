7616e6b74f952497cb27c89b9898ed007ea0631f
Eduardo Ramos <eduardoramos15975@gmail.com>
2024-08-08 14:03:29 -0300

    Referenciando os posts do usuário

--- src/main/java/com/eduardo/workshopmongo/domain/User.java
+++ src/main/java/com/eduardo/workshopmongo/domain/User.java
@@ -1,17 +1,16 @@
 package com.eduardo.workshopmongo.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
 @Document
@@ -22,4 +21,12 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
